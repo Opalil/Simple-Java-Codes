31e1239f4021b33df9b53f8b8a88a7cae64b26e5
Opalil <Opalil@users.noreply.github.com>
2020-11-13 11:06:50 +0200

    Read rss-feed and show open jobs
    Read rss-feed and show open jobs from Kuntarekry/Kuopio.

---
+++ Sorting-Algorithms/src/KuntarekryKuopio.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class KuntarekryKuopio {
+		
+	public static void main(String[]args) throws IOException {
+		// Kuopion kaupungin työpaikat Kuntarekry-järjestelmässä
+		String url = "https://www.kuntarekry.fi/fi/tyopaikat/?organisation=12&lang=fi_FI&sort=-changetime&limit=500&format=rss";
+		
+		URL uri = new URL(url);
+		//uri = uri.replaceFirst("\\<[^>]*>","");
+		HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/xml");
+		
+		DocumentBuilderFactory fact = DocumentBuilderFactory.newInstance();
+		
+		try {
+			
+			InputStream xml = conn.getInputStream();
+			DocumentBuilder builder = fact.newDocumentBuilder();
+			Document doc = builder.parse(xml);
+			
+			NodeList jobs = doc.getElementsByTagName("title");
+			NodeList descr = doc.getElementsByTagName("description");
+			NodeList links = doc.getElementsByTagName("link");
+
+			
+			for(int i = 0; i < jobs.getLength(); i++) {
+				// Title
+				String title = jobs.item(i).getTextContent();
+				System.out.println("Title: " + title);
+				// Description
+				String desc = descr.item(i).getTextContent();
+				desc = desc.replaceAll("\\<[^>]*>",""); // Get rid of html-tags
+				System.out.println("Desc: " + desc);
+				// Link
+				String link = links.item(i).getTextContent();
+				System.out.println("Link: " + link + "\n");
+			}
+			
+		} catch(SAXException | IOException ie) {
+			ie.printStackTrace();
+		} catch(ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
